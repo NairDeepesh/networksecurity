97500387bb08eed78d957768c2c54eeb0689f47d
NairDeepesh <deepeshnair05@gmail.com>
2025-03-13 23:26:06 +0530

    Model pusher added

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -87,11 +87,11 @@ class DataTransformation:
             test_arr = np.c_[ transformed_input_test_feature, np.array(target_feature_test_df) ]
 
             #save numpy array data
-            save_numpy_array_data( self.data_transformation_config.transformed_train_file_path, array=train_arr, )
-            save_numpy_array_data( self.data_transformation_config.transformed_test_file_path,array=test_arr,)
-            save_object( self.data_transformation_config.transformed_object_file_path, preprocessor_object,)
+            save_numpy_array_data( self.data_transformation_config.transformed_train_file_path, array=train_arr )
+            save_numpy_array_data( self.data_transformation_config.transformed_test_file_path,array=test_arr)
+            save_object( self.data_transformation_config.transformed_object_file_path, preprocessor_object)
 
-            #save_object( "final_model/preprocessor.pkl", preprocessor_object,)
+            save_object( "final_model/preprocessor.pkl", preprocessor_object)
 
 
             #preparing artifacts
